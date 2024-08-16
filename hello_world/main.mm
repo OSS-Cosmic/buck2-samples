832bf222305be8629885030b8e770905b9452fb7
Michael Pollind <mpollind@gmail.com>
2024-08-15 21:20:05 -0700

    feat: add sammple
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

---
+++ hello_world/main.mm
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  test
+//
+//  Created by Michael Pollind on 8/14/24.
+//
+
+#import <Foundation/Foundation.h>
+#include "library.hpp"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    print_hello();
+    return 0;
+}
+
